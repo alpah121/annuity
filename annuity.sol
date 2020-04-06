5e97ad627c20fffba7fd66bd1674330f873227e1
jacob <jacob_a_reed@yahoo.com>
2020-04-06 23:59:50 +0000

    almost done with constructor

--- annuity.sol
+++ annuity.sol
@@ -16,11 +16,14 @@
 pragma ^0.4.4;
 
 contract annuity {
-uint recommendedPayment = 0.003;
 uint monthlyWithdraw = 1;
+int totalInAccount = 0;
+uint term;
 enum Status {deposit, confirm, withdraw}
 Status public status;
-
+address owner payable;
+address manager payable;
+manager = ;
 struct Payment {
 	uint timestamp;
 	uint amount;
@@ -31,14 +34,39 @@ uint totalPayments;
 
 function deposit() public payable
 	{
+	require(status == Status.deposit);
 	require(msg.value > 0);
 	payments.push(Payment(block.timestamp, msg.value));
 	totalPayments += 1;
+	totalInAccount += msg.value;
 	}
 	
 function withdraw() public 
 	{
+	require(msg.sender == owner);
 	require(status == Status.withdraw);
+	require(totalInAccount >= monthlyWithdraw);
+	totalInAccount -= monthlyWithdraw;
+	owner.transfer(monthlyWithdraw);
+	}
 	
+constructor(uint _term, address _owner) public payable 
+	{
+	require(msg.sender == manager);
+	require(msg.value > 0);
+	require(_term >= 12);
+	createdOn = now;
+	term = _term;
+	endOfTerm = now + (term * 1 minutes);
+	owner = _owner;
+	payments.push(Payment(block.timestamp, msg.value));
+	totalPayments += 1;
+	totalInAccount += msg.value;
+	status = Status.deposit;
+	}
+	
+function confirm(address _owner)
+	{
+	require()	
 	}
 }
