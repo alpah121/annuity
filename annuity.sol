7932622801e7606de13adf6a51a8c06c77617987
jacob <jacob_a_reed@yahoo.com>
2020-04-06 18:08:01 +0000

    added annuity.sol and design

---
+++ annuity.sol
@@ -0,0 +1,21 @@
+//annuity.sol
+/*
+<li>deposit</li>
+<li>withdraw</li>
+<li>constructor</li>
+<li>isOnTrack</li>
+<li>borrow</li>
+<li>deathOccured</li>
+<li>highRiskWithdraw</li>
+<li>lowRiskWithdraw</li>
+<li>calculateSplit</li>
+<li>settings</li>
+*/
+
+
+pragma ^0.4.4;
+
+contract annuity {
+uint monthlyPayment = 300;
+uint 
+}
